2cebfc5ddd2444a82c98ecfed1d3d1f0ba0c33eb
Teslim <steslimolayiwola@gmail.com>
2024-05-05 20:46:55 +0100

    Background components

--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -57,43 +57,6 @@ const Home = () => {
                     </button>
                   </div>
                 </div>
-                <div className="pt-[10rem] flex gap-10">
-                  <button>
-                    <Link
-                      to={"/about"}
-                      handleClick={() => setPageSteps("about")}
-                    >
-                      <p className=" flex font-roboto w-[100px] justify-center font-light p-[5px]   border-[3px] border-[#ccc] shadow-sm shadow-[gray] whitespace-nowrap">
-                        About Me
-                      </p>
-                    </Link>
-                  </button>
-
-                  <button>
-                    <Link>
-                      <p className="flex font-roboto w-[100px] justify-center font-light p-[5px]   border-[3px] border-[#ccc] shadow-sm shadow-[gray] whitespace-nowrap">
-                        Skills
-                      </p>
-                    </Link>
-                  </button>
-
-                  <button>
-                    {" "}
-                    <Link>
-                      <p className=" flex font-roboto w-[100px] justify-center font-light p-[5px]   border-[3px] border-[#ccc] shadow-sm shadow-[gray] whitespace-nowrap">
-                        Projects
-                      </p>
-                    </Link>
-                  </button>
-                  <button>
-                    {" "}
-                    <Link>
-                      <p className=" font flex font-roboto w-[100px] justify-center font-light p-[5px]   border-[3px] border-[#ccc] shadow-sm shadow-[gray] whitespace-nowrap">
-                        Links
-                      </p>
-                    </Link>
-                  </button>
-                </div>
               </div>
             )}
           </div>
