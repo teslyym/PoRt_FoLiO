d57bd268e54bf0c91f9fe6ec311dc4eaab2b5ebb
Teslim <steslimolayiwola@gmail.com>
2024-05-14 17:12:39 +0100

    Addition of Programming Languages skills

--- src/assets/Components/Skills.jsx
+++ src/assets/Components/Skills.jsx
@@ -8,20 +8,33 @@ const Skills = () => {
       <div>
         <Header />
         <Background />
-        <div className="w-[700px] pt-[2rem] mb-[1rem] p-5 gap-4">
+        <div className=" pt-[2rem] mb-[1rem] p-5 gap-4">
           <h1 className="font-poppins font-bold text-2xl">Skills</h1>
 
-          <div className="flex gap-5 border border-solid justify-around w-[800px]">
+          <div className="flex mt-[2rem] gap-5 border-[2px] table--shadow border-solid justify-around w-full">
             <div>
-              <p>Programming Languages</p>
+              <p className="font-poppins text-sm font-bold">
+                Programming Languages
+              </p>
+
+              <h1>JavaScript</h1>
             </div>
             <div className=" bg-[gray] w-[1.5px]"></div>
             <div>
-              <p>Web Frontend</p>
+              <p className="font-poppins text-sm font-bold">Web Frontend</p>
+              <h1 className="font-roboto text-sm font-normal">HTML</h1>
+              <h1>CSS</h1>
+              <h1>React</h1>
+              <h1>Tailwind</h1>
+              <h1>Sass</h1>
             </div>
-
+            <div className=" bg-[gray] w-[1.5px]"></div>
             <div>
-              <p>Web Backend</p>
+              <p className="font-poppins text-sm font-bold">Web Backend</p>
+              <h1>Node</h1>
+              <h1>Express</h1>
+              <h1>Django</h1>
+              <h1></h1>
             </div>
           </div>
         </div>
