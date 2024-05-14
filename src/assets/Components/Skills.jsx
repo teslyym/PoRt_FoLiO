ded13f40ceb7e18bd8708086910d12cdf87894bc
Teslim <steslimolayiwola@gmail.com>
2024-05-14 16:34:55 +0100

    addition of skills content

--- src/assets/Components/Skills.jsx
+++ src/assets/Components/Skills.jsx
@@ -10,6 +10,20 @@ const Skills = () => {
         <Background />
         <div className="w-[700px] pt-[2rem] mb-[1rem] p-5 gap-4">
           <h1 className="font-poppins font-bold text-2xl">Skills</h1>
+
+          <div className="flex gap-5 border border-solid justify-around w-[800px]">
+            <div>
+              <p>Programming Languages</p>
+            </div>
+            <div className=" bg-[gray] w-[1.5px]"></div>
+            <div>
+              <p>Web Frontend</p>
+            </div>
+
+            <div>
+              <p>Web Backend</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
